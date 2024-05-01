a99dc3b1abbe34f4c2c5b0382c93fb32bdc9dc65
Weverton Vitor <wevertonvitor01@gmail.com>
2024-05-01 14:36:42 -0300

    Dinamic images

--- lib/motivator_web/controllers/page_controller.ex
+++ lib/motivator_web/controllers/page_controller.ex
@@ -2,19 +2,33 @@ defmodule MotivatorWeb.PageController do
   use MotivatorWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    json_path = "/media/weverton/D/Documentos/Faculdade/3º Periodo/PROGRAMACAO FUNCIONAL /motivator/lib/data/frases.json"
-    {:ok, json_content} = File.read(json_path)
-    case Jason.decode(json_content) do
-      {:ok, json_data} ->
-        random_item = Enum.random(json_data)
-        render(conn, :home, layout: false, data: random_item)
-        {:error, _} ->
+    phrases_path = "lib/data/frases.json"
+    images_path = "lib/data/imagens.json"
+
+    case ler_json(phrases_path) do
+      {:ok, json_phrases} ->
+        case ler_json(images_path) do
+          {:ok, json_images} ->
+            random_phrase = Enum.random(json_phrases)
+            random_image = Enum.random(json_images)
+            render(conn, :home, phrase: random_phrase, image: random_image)
+          {:error, _} ->
+            conn
+            |> put_status(500)
+            |> text("Erro ao ler o JSON")
+        end
+      {:error, _} ->
         conn
         |> put_status(500)
-        |> text("Erro ao decodificar o JSON")
+        |> text("Erro ao ler o JSON")
     end
+  end
 
+  defp ler_json(json_path) do
+    {:ok, json_content} = File.read(json_path)
+    case Jason.decode(json_content) do
+      {:ok, json_data} -> {:ok, json_data}
+      {:error, _} -> {:error, :decodificacao}
+    end
   end
 end
